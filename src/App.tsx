0dbfdea02d283fec7da254aac539f272ab45dca3
Kris <krispyassets@gmail.com>
2026-01-21 02:32:29 +0700

    Event infinite loading guard

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import EventsIndex from "./pages/EventsIndex";
 import EventDetail from "./pages/EventDetail";
 import SearchPage from "./pages/SearchPage";
@@ -13,6 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<EventsIndex />} />
         <Route path="/event/:eventId" element={<EventDetail />} />
+        <Route path="/event" element={<Navigate to="/" replace />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/credits" element={<CreditsPage />} />
